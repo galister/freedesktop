29b11bab12881cc0eb5f5522bd85f5de4f5365c9
Javier Feliz <javier0eduardo@hotmail.com>
2025-09-22 22:28:01 -0400

    Implemented icon overlays based on the spec

--- freedesktop-cli/src/main.rs
+++ freedesktop-cli/src/main.rs
@@ -1,39 +1,40 @@
 use freedesktop_apps::ApplicationEntry;
-use freedesktop_icon::IconTheme;
 
 fn main() {
-    // Icons not found (but should be I think)
-    // - yazi
-    // - Mattermost
-    let theme = IconTheme::current();
-    println!("Current icon theme: {}", theme.name());
-    // for app in ApplicationEntry::all() {
-    //     if app.should_show() {
-    //         println!("{}", app.name().unwrap());
-    //         if let Some(app_icon) = app.icon() {
-    //             println!("-- ICON --");
-    //             println!("{}", app_icon);
-    //             let icon = theme.get(&app_icon);
-    //             println!(
-    //                 "{}",
-    //                 icon.map_or_else(
-    //                     || "Not found".to_string(),
-    //                     |p| p.to_string_lossy().into_owned()
-    //                 )
-    //             );
-    //         }
-    //         println!();
-    //     }
-    // }
-
-    let icon = theme.get("yazi");
-    println!("{}", icon.unwrap().display());
-}
-
-fn print_theme_info(theme: IconTheme) {
-    println!("--- {} ---", theme.name());
-    println!("Default size: {}", theme.default_size().unwrap_or(1));
-    for i in theme.inherits() {
-        println!("INHERIT: {}", i);
+    for app in ApplicationEntry::all() {
+        if app.should_show() {
+            println!("{}", app.name().unwrap());
+            if let Some(app_icon) = app.icon() {
+                // println!("-- ICON --");
+                println!("{}", app_icon);
+                let icon = freedesktop_icon::get_icon(&app_icon);
+                println!(
+                    "{}",
+                    icon.map_or_else(
+                        || "Not found".to_string(),
+                        |p| p.to_string_lossy().into_owned()
+                    )
+                );
+            }
+            println!();
+        }
     }
+
+    // let icon = theme.get("brave-fcmiknabmjbdhccoehdfdbplhgbeccdl-Default");
+    // println!("{}", icon.unwrap_or("Not found".into()).display());
+    // println!(
+    //     "{}",
+    //     Pixmap::get("brave-fcmiknabmjbdhccoehdfdbplhgbeccdl-Default")
+    //         .unwrap_or("Not found".into())
+    //         .display()
+    // )
 }
+
+// fn print_theme_info(theme: IconTheme) {
+//     println!("--- {} ---", theme.name());
+//     println!("Default size: {}", theme.default_size().unwrap_or(1));
+//     for i in theme.inherits() {
+//         println!("INHERIT: {}", i);
+//     }
+//     println!("Path: {}", theme.path().display());
+// }
--- freedesktop-icon/src/lib.rs
+++ freedesktop-icon/src/lib.rs
@@ -1,5 +1,7 @@
 use ini::Ini;
-use std::{collections::HashSet, io::BufRead, path::PathBuf};
+use std::{collections::HashSet, io::BufRead, path::PathBuf, sync::LazyLock};
+
+static CURRENT_ICON_THEME: LazyLock<IconTheme> = LazyLock::new(|| IconTheme::current());
 
 #[derive(Debug, Clone)]
 pub struct IconTheme {
@@ -41,12 +43,20 @@ impl IconTheme {
         inherits.split(",").map(String::from).collect()
     }
 
+    // Per the spec, we will use $XDG_DATA_HOME/icons/[theme name]
+    // as an "overlay" if it exists. Meaning that if a theme
+    // has dir /some/system/install/path/[theme]/48x48
+    // we will look in $XDG_DATA_HOME/icons/[theme]/48x48
+    // if it exists before we look in the system path.
     pub fn icon_dirs(&self, size: u32, scale: u8) -> Vec<PathBuf> {
         let Some(dir_str) = &self.config_value("Icon Theme", "Directories") else {
             return Vec::new();
         };
 
         let dirs: Vec<String> = dir_str.split(",").map(String::from).collect();
+        let overlay_dir = freedesktop_core::xdg_data_home()
+            .join("icons")
+            .join(&self.name);
 
         let mut paths: Vec<PathBuf> = Vec::new();
 
@@ -62,6 +72,11 @@ impl IconTheme {
             };
 
             if (dir_size == &size) && (dir_scale == scale) {
+                let overlay = overlay_dir.join(d);
+                if overlay.exists() {
+                    paths.push(overlay);
+                }
+
                 paths.push(self.path.join(d));
             }
         }
@@ -76,76 +91,6 @@ impl IconTheme {
 
         size_str.parse::<u32>().ok()
     }
-
-    /// Get the full inheritance stack for a theme
-    /// by following its own Inherits= as well
-    /// as the Inherits of the themes it
-    /// inherits from.
-    /// INCLUSIVE: The stack will include the current
-    /// theme's name.
-    /// This function is mostly used internally,
-    /// but it is exposed in case you have a
-    /// special use case.
-    // pub fn inheritance_stack(&self) -> Vec<String> {
-    //     // We will do a depth first search over
-    //     // the inheritance chain
-    //     let mut seen: HashSet<String> = HashSet::new();
-    //     let mut search_stack: Vec<String> = Vec::new();
-    //     let mut stack: Vec<String> = Vec::new();
-
-    //     seen.insert(self.name.clone());
-
-    //     search_stack.extend(self.inherits().into_iter().rev().collect::<Vec<String>>());
-
-    //     while let Some(next) = search_stack.pop() {
-    //         if seen.contains(&next) {
-    //             println!("      Skipping already seen theme: {}", next);
-    //             continue;
-    //         }
-
-    //         seen.insert(next.clone());
-
-    //         let from_name_start = Instant::now();
-    //         let Some(theme) = IconTheme::from_name(next.clone()) else {
-    //             let from_name_time = from_name_start.elapsed();
-    //             println!(
-    //                 "      Failed to load theme '{}' in {:?}",
-    //                 next, from_name_time
-    //             );
-    //             continue;
-    //         };
-    //         let from_name_time = from_name_start.elapsed();
-    //         from_name_total += from_name_time;
-    //         themes_loaded += 1;
-    //         println!("      Loaded theme '{}' in {:?}", next, from_name_time);
-
-    //         let inherits_start = Instant::now();
-    //         let theme_inherits = theme.inherits();
-    //         let inherits_time = inherits_start.elapsed();
-    //         inherits_total += inherits_time;
-    //         println!(
-    //             "        Theme '{}' inherits {} themes, took {:?}",
-    //             next,
-    //             theme_inherits.len(),
-    //             inherits_time
-    //         );
-
-    //         search_stack.extend(theme_inherits.into_iter().rev().collect::<Vec<String>>());
-    //         stack.push(theme);
-    //     }
-
-    //     println!("      Loop processing took: {:?}", loop_start.elapsed());
-    //     println!("      Stats: {} themes loaded", themes_loaded);
-    //     println!("        Total from_name() time: {:?}", from_name_total);
-    //     println!("        Total inherits() time: {:?}", inherits_total);
-    //     println!(
-    //         "    Total inheritance_stack took: {:?}, returning {} themes",
-    //         total_start.elapsed(),
-    //         stack.len()
-    //     );
-
-    //     stack
-    // }
 
     // Internal function for getting an icon from the
     // theme. The public get() function actually
@@ -169,12 +114,10 @@ impl IconTheme {
         None
     }
 
-    /// Get an icon by name following the freedesktop icon theme specification
-    /// Searches through the current theme and inherited themes for the icon
-    pub fn get(&self, icon_name: &str) -> Option<PathBuf> {
-        let size = self.default_size().unwrap_or(48);
-        let scale: u8 = 1;
-
+    // First option in the process: We go through the current theme
+    // and its inherited themes all the way down to see if we
+    // can find the icon.
+    fn get_through_inheritance(&self, icon_name: &str, size: u32, scale: u8) -> Option<PathBuf> {
         if let Some(icon_path) = &self.get_icon(icon_name, size, scale) {
             return Some(icon_path.to_owned());
         }
@@ -186,13 +129,27 @@ impl IconTheme {
                 continue;
             };
 
-            match theme.get_icon(icon_name, size, scale) {
+            match theme.get_through_inheritance(icon_name, size, scale) {
                 Some(icon_path) => return Some(icon_path),
                 None => continue,
             }
         }
 
         None
+    }
+
+    /// Get an icon by name following the freedesktop icon theme specification
+    /// Searches through the current theme and inherited themes for the icon
+    pub fn get(&self, icon_name: &str) -> Option<PathBuf> {
+        let size = self.default_size().unwrap_or(48);
+        let scale: u8 = 1;
+
+        if let Some(path) = &self.get_through_inheritance(icon_name, size, scale) {
+            return Some(path.to_owned());
+        }
+
+        // Pixmaps are a last resort
+        Pixmap::get(icon_name)
     }
 }
 
@@ -279,17 +236,43 @@ impl IconTheme {
     }
 }
 
-// We'll use this function when we just need a line from
-// an index.theme without having to parse the whole file
-fn find_line(path: &str, prefix: &str) -> Option<String> {
-    let file = std::fs::File::open(path).ok()?;
-    let reader = std::io::BufReader::new(file);
+/// Pixmaps are a fallback when an icon is not found
+/// in a theme or any of it's inherited themes.
+pub struct Pixmap;
 
-    for line in reader.lines().filter_map(|l| l.ok()) {
-        if line.starts_with(prefix) {
-            return Some(line);
+impl Pixmap {
+    pub fn get(icon_name: &str) -> Option<PathBuf> {
+        let pixmap_paths = freedesktop_core::xdg_data_dirs()
+            .into_iter()
+            .map(|p| p.join("pixmaps"))
+            .filter(|p| p.exists());
+
+        let filenames = [
+            format!("{}.{}", icon_name, "svg"),
+            format!("{}.{}", icon_name, "png"),
+            format!("{}.{}", icon_name, "xpm"),
+        ];
+
+        for d in pixmap_paths {
+            for f in &filenames {
+                let icon_path = d.join(f);
+                if icon_path.exists() {
+                    return Some(icon_path);
+                }
+            }
         }
-    }
 
-    None
+        None
+    }
+}
+
+/// Convenience function that will:
+/// Get the current icon theme from IconTheme::current()
+/// Call theme.get() which will get the icon for
+/// the default size and scale set for the theme.
+/// IconTheme::current() will be cached using LazyLock
+/// so multiple calls to this function do not incurr
+/// a performance penalty
+pub fn get_icon(name: &str) -> Option<PathBuf> {
+    CURRENT_ICON_THEME.get(name)
 }
